b233d9591aa8d84b1464e28db111f3ed04dc4998
Dmitry <Agodimon@mail.ru>
2021-05-08 00:48:36 +0500

    doing my homework 14. write tests.

--- Main.java
+++ Main.java
@@ -1,17 +1,10 @@
 
+
 import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int[] arraysInt = {1, 2, 3, 6, 8, 7, 5, 8, 6, 9, 2, 5, 8, 4, 5, 5, 8, 7, 5};
-        System.out.println(pullOutElemOfArr(arraysInt));
-
-        int[] array={4,4,1,};
-        System.out.println(getCheckElemOfArr(array));
-    }
-
-    private static boolean getCheckElemOfArr(int[] array) {
+    public boolean getCheckElemOfArr(int[] array) {
         boolean allCheck=false, s=false, t =true;
         int u=array[0];
         for (int j : array) {
@@ -23,8 +16,7 @@ public class Main {
         return allCheck;
     }
 
-
-    private static String pullOutElemOfArr(int[] arraysInt) throws RuntimeException {
+    public int[] pullOutElemOfArr(int[] arraysInt) throws RuntimeException {
         if (findElemInArr(arraysInt)){
             for (int j = arraysInt.length - 1; j >= 0; j--) {
                 if (arraysInt[j] == 4) {
@@ -33,10 +25,10 @@ public class Main {
                 }
             }
         }else {throw new RuntimeException("В массиве нет четвёрок");}
-        return Arrays.toString(arraysInt);
+        return arraysInt;
     }
 
-    private static boolean findElemInArr(int[] arr) {
+    public boolean findElemInArr(int[] arr) {
         for (int j : arr) if (j == 4) return true;
         return false;
     }
